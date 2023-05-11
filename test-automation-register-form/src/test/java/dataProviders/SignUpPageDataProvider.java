6bd70ad4be65a5d81bcc6f279e8b2944294804c4
cristiantabira <62101240+cristiantabira@users.noreply.github.com>
2023-05-11 21:34:25 +0300

    --Data_Provider_Example

---
+++ test-automation-register-form/src/test/java/dataProviders/SignUpPageDataProvider.java
@@ -0,0 +1,16 @@
+package dataProviders;
+
+import org.testng.annotations.DataProvider;
+
+public class SignUpPageDataProvider {
+    @DataProvider(name="signUpPageDataProvider")
+    public Object[][] SignUpPageDataProvider()
+    {
+        return new Object[][]
+                {
+                        {"chrome"},
+                        {"firefox"},
+                        {"edge"}
+                };
+    }
+}
