65a66c782c49193e9e28cef9540c6d571c3d0d65
cristiantabira <62101240+cristiantabira@users.noreply.github.com>
2023-05-11 20:17:53 +0300

    --initial_setup

---
+++ test-automation-register-form/src/test/java/Tests/BrowserUITest.java
@@ -0,0 +1,26 @@
+package Tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class BrowserUITest {
+
+    WebDriver driver;
+    String url;
+    String browser;
+
+    @BeforeClass
+    public void setUp()
+    {
+        WebDriverManager.chromedriver().setup();
+    }
+    @Test
+    public void SearchOnGoogleTest()
+    {
+        driver = new ChromeDriver();
+        driver.get("http://google.com");
+    }
+}
